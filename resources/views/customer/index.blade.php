7fbdcf0f137e6b3eb7c59054feb9e1bb6826473d
ULTRON\aroabarberan <aroabarberan>
2017-10-13 18:03:52 +0200

    Install npm. Import file style.scss to app.scss and add styles

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,6 +1,21 @@
 @extends('base')
-@section('title', 'Customer List')
+@section('title', 'Customers List')
 
 @section('main')
-
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Avatar</th>
+        </tr>
+        @foreach($customers as $customer)
+            <tr>
+                <td>{{ $customer->id }}</td>
+                <td>{{ $customer->name }}</td>
+                <td>{{ $customer->lastname }}</td>
+                <td><img class="avatar" src="/img/{{$customer->avatar}}" alt=""></td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
